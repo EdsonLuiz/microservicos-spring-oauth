8dcabb0cdbefa106cc785919f19a61f873a8a4aa
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-21 09:31:00 -0300

    Start project

---
+++ src/main/java/com/edson/app/oauth/ServicoOauthApplication.java
@@ -0,0 +1,13 @@
+package com.edson.app.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoOauthApplication.class, args);
+	}
+
+}
