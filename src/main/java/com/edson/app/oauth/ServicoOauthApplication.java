3e57dffcad3d67146d8d90bf95d1c39d48d104c9
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-25 11:55:09 -0300

    Refactor to show some encrypted data on startup

--- src/main/java/com/edson/app/oauth/ServicoOauthApplication.java
+++ src/main/java/com/edson/app/oauth/ServicoOauthApplication.java
@@ -1,17 +1,34 @@
 package com.edson.app.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class ServicoOauthApplication {
+public class ServicoOauthApplication implements CommandLineRunner {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ServicoOauthApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String password = "12345";
+		
+		for (int i = 0; i < 4; i++) {
+			String passwordBcrypt = passwordEncoder.encode(password);
+			System.out.println(passwordBcrypt);
+		}
+		
 	}
 
 }
